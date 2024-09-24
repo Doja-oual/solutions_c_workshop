50c92f427771e101ef73a3564c6fddf66b78dfb2
Doja-oualla <dojaoualla@gmail.com>
2024-09-24 15:18:21 +0200

    challeng1

---
+++ Day01/Variables/chaleng 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int age;
+    char nom[50] ,prénom[50],email[100],sixe[50];
+    printf("donne le nome ");
+    scanf("%s",&nom);
+    printf("donne le prénom ");
+    scanf("%s",&prénom);
+    printf("donne le sixe");
+    scanf("%s",&sixe);
+    printf("donne le email ");
+    scanf("%s",&email);
+    printf("donne le age ");
+    scanf("%d",&age);
+   
+    
+    return 0;
+    
+    
+}
