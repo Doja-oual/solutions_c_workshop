630ebc38ff7e8de567c2b76300c2d1b38fb78f2b
Doja-oualla <dojaoualla@gmail.com>
2024-09-28 18:03:56 +0200

    blocage a challeng 7

--- Day02/lfonctions/challeng7.c
+++ Day02/lfonctions/challeng7.c
@@ -1,8 +1,21 @@
 //Challenge 7 : Fonction d'Inversion de Chaîne
 #include<stdio.h>
 #include<stdlib.h>
-char invrsChain(char  caracter);
+void inversionChain(char caract[50]){
+char X;
+for (int i = 0; i < 50-1-i; i++){
+    X=caract[i];
+   caract[i]= caract[50-1-i];
+   caract[50-1-i]=X;
+    }
+}
 
 int main(){
+    char caract[50],inverscaract[50];
+    printf(" Entrez une chaîne de caractère :  ");
+    gets(caract);
+    inverscaract[50]= inversionChain( caract[50]);
+    printf(" Entrez une chaîne de caractère : %s ",inverscaract[50]);
+
     return 0;
 }
